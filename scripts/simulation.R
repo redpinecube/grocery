23f58375a0879e9e2c0c0c1222b2b024c7fa6eeb
Tara Chakkithara <113033661+redpinecube@users.noreply.github.com>
2024-11-21 21:48:23 -0500

    bug fixes simulation.R

--- scripts/simulation.R
+++ scripts/simulation.R
@@ -2,11 +2,11 @@
 ### vendor, brand, egg size, month, and price. 
 
 set.seed(123)
-avg_size <- sample(c("Medium", "Small", "Large", "Jumbo"), 100, replace = TRUE)
+size <- sample(c("Medium", "Small", "Large", "Jumbo"), 100, replace = TRUE)
 avg_price <- runif(100, min = 0, max = 15)
 brand <- sample(c("a", "b", "c"), 100, replace = TRUE)
 vendor <- sample(c("vendor1", "vendor2", "vendor3"), 100, replace = TRUE)
 month <- sample(1:12, 100, replace = TRUE)
 
-data.frame(avg_price, avg_size, brand, month, vendor)
+data.frame(avg_price, size, brand, month, vendor)
 
