f686912bb9c77326f67073c5cbce134859abf211
pixel-echo <tara.chakkithara@icloud.com>
2024-12-03 02:27:00 -0800

    improved data cleaning

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -1,118 +1,66 @@
+
 library(tidyverse)
 product <- read_csv("./data/raw/hammer-4-product.csv")
-price <- read_csv("./data/raw/hammer-4-raw.csv")
 
 product <- product |>
-  select(id, vendor, product_name, units, brand)
-
-price <- price |>
-  select(nowtime, current_price, price_per_unit, other, product_id) |>
-  rename(time = nowtime, price = current_price, id = product_id)
-
-data <- merge(price, product, by = "id", all.x = TRUE)
-
-voila_data <- data |>
-  filter(vendor == "Voila") |>
-  mutate(product_name = str_to_lower(product_name)) |>
-  filter(grepl("egg ", product_name)) |>
-  filter(grepl("white ", product_name) | grepl("brown ", product_name)) |>
-  filter(grepl("large ", product_name) | grepl("medium", product_name)) |>
-  mutate(price_per_unit = as.numeric(str_extract(price_per_unit, "\\d+\\.\\d+"))) |>
+  filter(vendor %in% c("Loblaws", "Metro", "Voila")) |>
+  filter(grepl("Eggs ", product_name)) |>
+  filter(grepl("White ", product_name) | grepl("Brown ", product_name)) |> 
   mutate(color = case_when(
-    grepl("white ", product_name) ~ "white",
-    grepl("brown ", product_name) ~ "brown"
+    grepl("White ", product_name) ~ "White",
+    grepl("Brown ", product_name) ~ "Brown"
   )) |>
   mutate(size = case_when(
-    grepl("extra large", product_name) ~ "extra large",
-    grepl("large", product_name) ~ "large",
-    grepl("medium", product_name) ~ "medium"
+    grepl("Extra Large ", product_name) ~ "Extra Large",
+    grepl("Jumbo ", product_name) ~ "Extra Large",
+    grepl("Large ", product_name) ~ "Large",
+    TRUE ~ "Medium"
   )) |>
-  mutate(type = case_when(
-    grepl("organic ", product_name) ~ "organic",
-    TRUE ~ "none"
-  )) |>
-  mutate(brand = case_when(
-    grepl("gold", product_name) ~ "Gold Egg",
-    grepl("gray", product_name) ~ "Gray Ridge"
-  )) |>
-  mutate(vendor = str_to_lower(vendor)) |>
-  select(color, brand, size, type, price_per_unit, time, vendor, other)
+  rename(product_id = id)
+
+price <- read_csv("./data/raw/hammer-4-raw.csv")
 
 
-problem_words <- c("Chocolate", "Eggo", "Frozen", "Egg White", "Egg-Shaped",
-                   "Pasta", "Noodles", "Eggplant", "Cookies", "Duck", "Brownies",
-                   "Cheese", "Egg Nog", "Liquid", "Bowl", "Candy", "Bagels",
-                   "Pickled", "Easter", "Onion", "Loaf", "Scrambled", "Replacer",
-                   "Eggnog", "Plant", "Almond", "Bento", "Quail", "Hard", "Noodle",
-                   "Pizza", "Cake", "Breakfast", "Oatmeal", "Pain")
-
-loblaws_data <- data |>
-  filter(vendor == "Loblaws") |>
-  filter(grepl("Eggs", product_name)) |>
-  filter(!apply(sapply(problem_words, function(word) grepl(word, product_name)), 1, any)) |>
-  filter(!(grepl("Egg Creations!", product_name) | grepl("Mini", product_name))) |>
-  mutate(price_per_unit = as.numeric(gsub("\\$|/1ea|/un.|/100un.", "", price_per_unit))) |>
-  mutate(size = case_when(
-    grepl("Extra", product_name) ~ "extra large",
-    grepl("Jumbo", product_name) ~ "extra large",
-    grepl("Large", product_name) ~ "large",
-    TRUE ~ "medium"
-  )) |>
-  mutate(type = case_when(
-    grepl("Organic", product_name) ~ "organic",
-    grepl("Free Run", product_name) ~ "free run",
-    grepl("Free Range", product_name) ~ "free range", 
-    TRUE ~ "none"
-  )) |>
-  mutate(color = case_when(
-    grepl("White", product_name) ~ "white",
-    grepl("Brown", product_name) ~ "brown"
-  )) |>
-  mutate(vendor = str_to_lower(vendor)) |>
-  select(color, brand, size, type, price_per_unit, time, vendor, other)
-
-
-metro_data <- data |>
-  filter(vendor == "Metro") |>
-  filter(grepl("Egg", product_name)) |>
-  filter(!apply(sapply(problem_words, function(word) grepl(word, product_name)), 1, any)) |>
-  mutate(size = case_when(
-    grepl("Extra", product_name) ~ "extra large",
-    grepl("Jumbo", product_name) ~ "extra large",
-    grepl("Large", product_name) ~ "large",
-    TRUE ~ "medium"
-  )) |>
-  mutate(type = case_when(
-    grepl("Organic", product_name) ~ "organic",
-    grepl("Free Run", product_name) ~ "free run",
-    grepl("Free Range", product_name) ~ "free range", 
-    TRUE ~ "none"
-  )) |>
-  mutate(color = case_when(
-    grepl("White", product_name) ~ "white",
-    grepl("Brown", product_name) ~ "brown"
-  )) |>
-  mutate(vendor = str_to_lower(vendor)) |>
-  select(color, brand, size, type, price_per_unit, time, vendor, other) |>
-  mutate(price_per_unit = case_when(
-    grepl("/100un.", price_per_unit) ~ as.numeric(gsub("\\$|/100un.", "", price_per_unit)) / 100,
-    grepl("/un.", price_per_unit) ~ as.numeric(gsub("\\$|/un.", "", price_per_unit))
-  ))
-
-
-data <- bind_rows(voila_data, loblaws_data, metro_data)
-
+data <- merge(price, product, by = "product_id", all.y = TRUE)
 
 data <- data |>
-  filter(other == "Out of Stock") |>
-  mutate(
-    other = case_when(
-      grepl("sale", other) ~ "Sale",
-      grepl("SALE", other) ~ "Sale",
-      TRUE ~ "No Sale"
-    )
-  )
+  select(price_per_unit, vendor, nowtime, color, size, product_name, brand, other) |>
+  mutate(type = case_when(
+    grepl("Organic", product_name) ~ "organic",
+    grepl("Free Run", product_name) ~ "free run",
+    grepl("Free Range", product_name) ~ "free range", 
+    TRUE ~ "none"
+  )) |>
+  filter(!(grepl("Oatmeal", product_name))) |>
+  mutate(brand = case_when(
+    grepl("Gold Egg", product_name) ~ "Gold Egg",
+    grepl("Compliments", product_name) ~ "Compliments",
+    grepl("Gray Ridge", product_name) ~ "Gray Ridge",
+    grepl("Conestoga", product_name) ~ "Conestoga Eggs", 
+    grepl("Longo's", product_name) ~ "Longo's",
+    grepl("Blue Menu", product_name) ~ "Blue Menu",
+    TRUE ~ brand
+  )) |>
+  filter(!(grepl("100g", product_name))) |>
+  mutate(price_per_unit = case_when(
+    grepl("/un.", price_per_unit) ~ as.numeric(gsub("\\$|/un.|\\s", "", price_per_unit)),
+    grepl("/1ea", price_per_unit) ~ as.numeric(gsub("\\$|/1ea|\\s", "", price_per_unit)),    
+    grepl("/item", price_per_unit) ~ as.numeric(gsub("\\$|/item|\\s", "", price_per_unit)),
+    grepl("fop.price.per.each", price_per_unit) ~ as.numeric(str_extract(price_per_unit, "\\d+\\.\\d+"))
+  )) |>
+  mutate(sale = case_when(
+    other == "SALE" ~ "Sale",
+    other == "sale\n$6.49" ~ "Sale",
+    TRUE ~ "No Sale"
+  )) |>
+  select(!other) |>
+  rename(time = nowtime) |>
+  filter(!(brand == "Life Smart")) |>
+  mutate(brand = case_when(
+    brand == "GoldEgg" ~ "Gold Egg",
+    TRUE ~ brand
+  )) |>
+  select(!product_name)
 
 write_csv(data, "./data/clean/clean_data.csv")
-
 
