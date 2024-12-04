c11290ab161b04ba4f1f389fb18cfe4768d33425
pixel-echo <tara.chakkithara@icloud.com>
2024-12-03 17:01:03 -0800

    results draft

--- scripts/learn_model.R
+++ scripts/learn_model.R
@@ -3,9 +3,8 @@ data <- read_csv("./data/clean/preprocess.csv")
 set.seed(45)
 
 data <- data |>
-  select(sale, voila, loblaws, brown, goldegg,
-         bluemenu, compliments, longos, grayridge, 
-         large, medium, none, organic, freerange, month, price_per_unit)
+  select(sale, voila, brown, 
+         large, organic, price_per_unit)
 
 shuffled_data <- data[sample(nrow(data)), ]
 train_indices <- 1:floor(0.8 * nrow(shuffled_data))
@@ -16,5 +15,6 @@ train_model <- lm(price_per_unit ~ ., data = train_data)
 test_predictions <- predict(train_model, newdata = test_data)
 actual_values <- test_data$price_per_unit
 rmse <- sqrt(mean((actual_values - test_predictions)^2))
-
+print(rmse)
 saveRDS(train_model, file = "./model/model.rds")
+
